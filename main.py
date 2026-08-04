300a0a987c98dabd9d04d53a7ced205103e571df
Iagoscastro16 <Iagoscastro16@gmail.com>
2026-08-04 12:56:21 -0300

    inicia estrutura HTMX/Jinja2 para frontend
    rotas UI,templates de listagem e detalhe de problems

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 from database.tables import createTables
 #importação do fastAPI
 from fastapi import FastAPI
+# Importação do staticfiles para o html
+from fastapi.staticfiles import StaticFiles
 #importação da rota do problems_categories
 from routes.problems_categories import router as problems_categories_router
 #importação da rota de categories
@@ -11,10 +13,14 @@ from routes.problems import router as problems_router
 # importação da rota shortcut
 from routes.shortcut import router as shortcut_router 
 
+
+
 # Criação das tabelas se não existirem
 createTables()
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="./src/static"), name="static")
 
 app.include_router(problems_categories_router)
 app.include_router(categories_router)
---
+++ routes/ui/problems.py
@@ -0,0 +1,24 @@
+# Importa o APIRouter, que serve para agrupar todas essas rotas e depois plugar no main.py
+from fastapi import APIRouter, Request
+# Importação da lógica do négocio
+from functions.support.problems import listProblemsByTitle,get_problem
+
+from src.templates_config import templates
+
+router = APIRouter()
+
+# Rota de listagem dos problemas
+
+@router.get("/ui/problems")
+def show_problems_list(request: Request):
+    result = listProblemsByTitle()
+    return templates.TemplateResponse("problem_list_by_title.html",{
+        "request": request,
+        "problemas":result["data"]
+    })
+
+@router.get("/ui/problems/{problem_id}")
+def getting_problems(request: Request, problem_id:int):
+    result = get_problem(problem_id)
+
+    result = templates.TemplateResponse("")
